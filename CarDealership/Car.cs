b89d9608f108f106bb9eac9bb10c929844c76adc
sergeiklein <sjklein088@gmail.com>
2023-02-18 14:25:22 -0800

    Add project files.

---
+++ CarDealership/Car.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarDealership
+{
+    public class Car//--------------------------------------------------------------------------created a class----------------------------------------
+    {
+        //fields are private class members
+        //THink of fields as local variables that 
+        //exist throughout your class
+        //These fields will have access to the methods 
+        //and constructors in your class
+        //fields is where actaul data being stored
+
+        private double price;//------------------------------------------------------------------created fields---------------------------------------
+        private int year;
+        private string model;
+        private string make;
+        private string color;
+        private double discount;
+        //declare fields as private
+        //provide public properties with get set methods to
+        //access and update that private field
+        //Think of a property as a combination of variable and a method that has Get and Set Methods
+        //THe get method returns the value of the variable
+        // Set method assigns the value
+        public double Price//---------------------------------------------------------------------creating properties---------------------------------
+        {
+            get
+            {
+                return price;   
+            }
+            set
+            {
+                if(value > 0) //General Variable built info VS
+                {
+                    price = value;
+                }
+            }
+        }
+        
+        public int Year { get; set; }   
+        public string Model { get; set; }
+        public string Make { get; set; }
+        public string Color { get; set; }
+        public double Discount { get; set; }
+       
+        // Create Constructor 
+        //should be same name as the class
+        public Car(double price, int year, string model, string make, string color)//---------------creating a constructor---------------------------
+        {
+            Price = price; 
+            Year= year;
+            Model = model;
+            Make = make;
+            Color = color;
+
+        }
+
+        public Car(double price)//-----------------------------------------------------------------creating another constructor----------------------
+        {
+            Price = price;
+        }
+
+        //Discount 30 percent of in the winter, 20 in the fall, 10 in the summer, 5 in the spring
+
+        public void SetPrice(double price, string season)// ---------------------------------------creating a method for season----------------------
+        {
+            switch(season)
+            {
+                case "fall":
+                    discount = .20;
+                    break;
+                case "winter":
+                    discount = .30;
+                    break;
+                case "summer":
+                    discount = .10;
+                    break;
+                case "spring":
+                    discount = .05;
+                    break;
+
+                default:
+                    Console.WriteLine("Enter a Valid season");
+                    break;
+            }
+            Price = price - (price * discount);
+        }
+     
+
+
+
+
+
+    }
+}
---
+++ CarDealership/Program.cs
@@ -0,0 +1,69 @@
+﻿// See https://aka.ms/new-console-template for more information
+using CarDealership;
+
+
+Car mycar = new Car(12000, 2015, "Honda", "Oddysey", "Grey");//created several instances of class
+
+Car car2 = new Car(22000, 2022, "Kia", "Soul", "Blue");
+
+Car car3 = new Car(32000, 2023, "Tesla", "NewOne", "White");
+
+Car car4 = new Car(45000, 2020, "Honda", "Pilot", "Black");
+
+Car car5 = new Car(8000, 1995, "Lexus", "Something", "Gold");
+
+Car car6 = new Car(17400, 2005, "Kia", "Sienna", "pink");
+
+List<Car> carInventory= new List<Car>();// reating of list 
+carInventory.Add(mycar);
+carInventory.Add(car2);
+carInventory.Add(car3);
+carInventory.Add(car4);
+carInventory.Add(car5);
+carInventory.Add(car6);
+
+Console.WriteLine(mycar.Price);
+
+mycar.SetPrice(mycar.Price, "winter");
+//foreach use with collections
+foreach (Car car in carInventory)
+    {
+        Console.WriteLine(car.Color);
+    }
+Console.WriteLine(mycar.Price);
+
+//// used if you know how many time you wnat to iterate
+//for (int i = 0; i < carInventory.Count; i++)
+//{
+//    Console.WriteLine(carInventory[i].Make);
+//}
+////Lets review loops
+//int num = 0;
+//while (num < 6)//must be true to excecute at all
+//{
+//    Console.WriteLine(num);
+//    num++;
+//}
+//int num2 = 0;
+//do //will alwasy excecute at least once. becasue its checking condition after excecution
+//{
+//    Console.WriteLine(num2);
+//    num2++;
+//} while (num2 < 6);
+
+
+
+
+
+
+
+
+
+
+//Car car2 = new Car(125000);
+
+//Car car3 = new Car(-12000);
+//Console.WriteLine(car3.Price);
+
+//mycar.SetPrice(mycar.Price, "winter");
+//Console.WriteLine(mycar.Price);
